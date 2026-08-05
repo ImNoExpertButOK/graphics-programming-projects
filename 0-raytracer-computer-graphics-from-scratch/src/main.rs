b8544769ef1aa5a52ef245de945d617e22a30195
renato <renatofreddi@gmail.com>
2026-08-05 11:17:27 -0300

    added basic vector struct and dot product

--- 0-raytracer-computer-graphics-from-scratch/src/main.rs
+++ 0-raytracer-computer-graphics-from-scratch/src/main.rs
@@ -1,7 +1,30 @@
 mod ppm;
 use ppm::PPMImage;
 
+#[derive(Copy, Clone)]
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3 { x, y, z }
+    }
+
+    fn dot(a: &Vec3, b: &Vec3) -> f64 {
+        a.x * b.x + a.y * b.y + a.z * b.z
+    }
+}
+
 fn main() {
     let o = PPMImage::new(600, 600, String::from("output"));
     o.save().expect("não foi possível criar o arquivo");
+
+    let v1 = Vec3::new(1.0, 2.0, 3.0);
+    let v2 = Vec3::new(4.0, 5.0, 6.0);
+
+    let result = Vec3::dot(&v1, &v2);
+    println!("Dot product: {}", result); // Output: 32.0
 }
